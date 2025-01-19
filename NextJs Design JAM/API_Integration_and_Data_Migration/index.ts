12abe80d8cb8df5e024ece1cb6f28abe0db738e9
SHIZA TARIQ KHAN <163668326+shiza13@users.noreply.github.com>
2025-01-19 12:55:32 +0500

    Add files via upload

---
+++ NextJs Design JAM/API_Integration_and_Data_Migration/index.ts
@@ -0,0 +1,7 @@
+import { type SchemaTypeDefinition } from 'sanity';
+import { Category } from './category'; // Path to your category schema
+import { product } from './product';   // Path to your product schema
+
+export const schema: { types: SchemaTypeDefinition[] } = {
+  types: [Category, product], // Add your schemas here
+};
